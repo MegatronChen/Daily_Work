ca67e4a94a1dd74fe6b603ae2e334abb61ccfd33
MegatronChen <kongfufatcat@163.com>
2018-05-22 09:33:37 +0800

    2018.05.22t0932HP-update to consider the repeat files from different subdirecroties

--- Daily_Work/2018.05.21FileExtractor.py
+++ Daily_Work/2018.05.21FileExtractor.py
@@ -1,5 +1,5 @@
 __author__ = 'Megatron_chen'
-
+print('\n')
 
 '''
 From a top directory, extract all files of a specified extension name(or severals in a tuple)
@@ -14,9 +14,15 @@ import tkinter
 from tkinter.filedialog import askdirectory
 
 
-TopDir = askdirectory(title='Choose the Top directory')
-TargetDir = askdirectory(title='Choose the Target directory')
-Target_FileExtension = ('.jpg','.jpeg')
+# TopDir = askdirectory(title='Choose the Top directory')
+# TargetDir = askdirectory(title='Choose the Target directory')
+# Target_FileExtension = ('.jpg','.jpeg')
+
+# TopDir = r'C:\Users\Megatron_chen\Desktop\Top'
+# TargetDir = r'C:\Users\Megatron_chen\Desktop\Target'
+# Target_FileExtension = ('.txt')
+
+FileNames = []
 
 Filecount = 0
 TopDir_Tree_Generator = os.walk(TopDir)
@@ -24,7 +30,17 @@ for EachPiece in TopDir_Tree_Generator:
     if len(EachPiece[2]):
         for EachFile in EachPiece[2]:
             if EachFile.endswith(Target_FileExtension):
-                shutil.copy(os.path.join(EachPiece[0],EachFile),TargetDir)
+                if EachFile not in FileNames:
+                    FileNames.append(EachFile)
+                    shutil.copy(os.path.join(EachPiece[0],EachFile),TargetDir)
+                else:
+                    RepeteIndex = FileNames.count(EachFile)
+                    (basename,fileExtension) = os.path.splitext(EachFile)
+                    TargetName = basename + '-Repeat-' + str(RepeteIndex) + fileExtension
+                    # print(TargetName)
+                    # print(os.path.join(TargetDir,TargetName,fileExtension))
+                    shutil.copyfile(os.path.join(EachPiece[0],EachFile),os.path.join(TargetDir,TargetName))
+
                 Filecount += 1
 
 print('Extraction Done! Totally {0} files!'.format(Filecount))
