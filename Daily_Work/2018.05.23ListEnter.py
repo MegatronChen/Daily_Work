5e911f54b64065505fdbbc033348d4806066cd29
MegatronChen <kongfufatcat@163.com>
2018-05-23 21:20:14 +0800

    2018.05.23t02120HP-Final version to list the file names from a directory to a txt file in a specified dir

--- Daily_Work/2018.05.23ListEnter.py
+++ Daily_Work/2018.05.23ListEnter.py
@@ -1,12 +1,30 @@
 __author__ = 'Megatron_chen'
 
 import os
+from tkinter.messagebox import *
 from tkinter.filedialog import askdirectory
 
 
 QueryDir = askdirectory(title='Choose the Query directory')
-Results = os.listdir(QueryDir)
+if QueryDir:
+    QureyResults = os.listdir(QueryDir)
 
-with open(r'C:\Users\Megatron_chen\Desktop\FileList.txt','w') as f:
-    for EachFile in Results:
-        f.write(EachFile +'\n')
+    TargetDir = askdirectory(title='Choose the target directory to generate the result file')
+
+    if TargetDir:
+        with open(os.path.join(TargetDir,'FileList.txt'),'w') as f:
+            for EachFile in QureyResults:
+                f.write(EachFile +'\n')
+
+        Info = showinfo('Process Report','Congratulations! The file names have been listed in the txt! ')
+
+    else:
+        warn = showwarning('DirLost','the Result dir has not been specified!')
+
+else:
+    warn = showwarning('DirLost','the Query dir has not been specified!')
+
+
+
+
+
